414b24b35ea6b8e6e67bfbb3d013f3d324e80d94
Nico Rieck <nico.rieck@gmail.com>
2011-03-06 20:01:48 +0100

    Remove unnecessary $LOAD_PATH modifications from spec_helper.
    Relative paths are not reliable (e.g. dm-dev), and bundler takes care of
    that anyway.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,7 @@
 require 'rubygems'
 require 'pathname'
 require 'fakeweb'
-
-# use local dm-core if running from a typical dev checkout.
-lib = File.join('..', '..', '..', 'dm-core', 'lib')
-$LOAD_PATH.unshift(lib) if File.directory?(lib)
-
-# use local dm-validations if running from a typical dev checkout.
-lib = File.join('..', '..', 'dm-validations', 'lib')
-$LOAD_PATH.unshift(lib) if File.directory?(lib)
 require 'dm-validations'
-
-# use local dm-serializer if running from a typical dev checkout.
-lib = File.join('..', '..', 'dm-serializer', 'lib')
-$LOAD_PATH.unshift(lib) if File.directory?(lib)
 
 # Support running specs with 'rake spec' and 'spec'
 $LOAD_PATH.unshift('lib') unless $LOAD_PATH.include?('lib')
